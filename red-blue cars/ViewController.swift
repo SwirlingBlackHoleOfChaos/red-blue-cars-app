4484cb21d7454928b2bf6854d770e6b822aeda04
SwirlingBlackHoleOfChaos <SwirlingBlackHoleOfChaos@gmail.com>
2016-06-25 18:34:54 -0400

    Initial Commit

---
+++ red-blue cars/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  red-blue cars
+//
+//  Created by Nadir Abdulhaqq on 6/25/16.
+//  Copyright © 2016 eyeBinary. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
